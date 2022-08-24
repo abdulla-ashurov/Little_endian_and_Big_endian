07feef025b121a7a4d645ef35787f0b4a9a87a65
abdulla.ashurov <abdulla.ashurov@dsr-corporation.com>
2022-08-24 10:36:33 +0500

    Add function swap_LE_BE_by_pointer

--- main.cpp
+++ main.cpp
@@ -11,12 +11,31 @@ bool is_LE_system()
     return *ptr == value_of_last_byte;
 }
 
+void swap_LE_BE_by_pointer(uint32_t& value) {
+    uint8_t *ptr = (uint8_t *)(&value);
+
+    for (int i = 0, j = sizeof(uint32_t) - 1; i < 2; i++, j--) {
+        uint8_t temp = ptr[i];
+        ptr[i] = ptr[j];
+        ptr[j] = temp; // or there can i use std::swap(ptr[i], ptr[j])
+    }
+}
+
 int main()
 {
     if (is_LE_system())
         std::cout << "CPU is a little endian" << std::endl;
     else
         std::cout << "CPU is a big endian" << std::endl;
+
+    uint32_t hex_number = 0x61626364;
+
+    std::cout << std::hex;
+    std::cout << "hex number is " << hex_number << std::endl;
+
+    swap_LE_BE_by_pointer(hex_number);
+
+    std::cout << "hex number is " << hex_number << std::endl;
 
     return 0;
 }
