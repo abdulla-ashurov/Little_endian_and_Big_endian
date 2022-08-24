e64edb852ad2d04cfb8e176256ba48d821f3b24c
abdulla.ashurov <abdulla.ashurov@dsr-corporation.com>
2022-08-24 13:20:13 +0500

    Add function swap LE BE by bitwise

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ bool is_LE_system()
     const uint32_t hex_number = 0x61626364;
     const uint8_t *ptr = (const uint8_t *)(&hex_number);
 
-    uint8_t sequence_of_hex_number_in_memory[4] = {0x64, 0x63, 0x62, 0x61};
+    uint8_t sequence_of_hex_number_in_memory[sizeof(uint32_t)] = {0x64, 0x63, 0x62, 0x61};
 
     for (size_t i = 0; i < sizeof(uint32_t); i++) {
         if (ptr[i] != sequence_of_hex_number_in_memory[i])
@@ -15,7 +15,7 @@ bool is_LE_system()
     return true;
 }
 
-void swap_LE_BE_by_pointer(uint32_t& value) {
+void swap_LE_BE_by_pointer(uint32_t &value) {
     uint8_t *ptr = (uint8_t *)(&value);
 
     for (int i = 0, j = sizeof(uint32_t) - 1; i < 2; i++, j--) {
@@ -23,6 +23,10 @@ void swap_LE_BE_by_pointer(uint32_t& value) {
         ptr[i] = ptr[j];
         ptr[j] = temp; // or there i can use std::swap(ptr[i], ptr[j])
     }
+}
+
+void swap_LE_BE_by_bitwise(uint32_t &value) {
+    value = (value << 24 | value >> 24) | ((value << 16 >> 24 << 16) | (value >> 16 << 24 >> 16));
 }
 
 int main()
@@ -33,12 +37,13 @@ int main()
         std::cout << "CPU is a big endian" << std::endl;
 
     uint32_t hex_number = 0x61626364;
-
     std::cout << std::hex;
     std::cout << "hex number is " << hex_number << std::endl;
 
     swap_LE_BE_by_pointer(hex_number);
+    std::cout << "hex number is " << hex_number << std::endl;
 
+    swap_LE_BE_by_bitwise(hex_number);
     std::cout << "hex number is " << hex_number << std::endl;
 
     return 0;
