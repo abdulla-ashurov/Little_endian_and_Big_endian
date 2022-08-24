ae25fb9628fd4f70696b14f2d9242a56c313facf
abdulla.ashurov <abdulla.ashurov@dsr-corporation.com>
2022-08-24 11:06:05 +0500

    Refactor code

--- main.cpp
+++ main.cpp
@@ -4,11 +4,15 @@ bool is_LE_system()
 {
     const uint32_t hex_number = 0x61626364;
     const uint8_t *ptr = (const uint8_t *)(&hex_number);
-    
-    // value of last byte from hex_number - 0x64 (hex number) equal to 'd' (char) symbol
-    const char value_of_last_byte = 'd';
 
-    return *ptr == value_of_last_byte;
+    uint8_t sequence_of_hex_number_in_memory[4] = {0x64, 0x63, 0x62, 0x61};
+
+    for (size_t i = 0; i < sizeof(uint32_t); i++) {
+        if (ptr[i] != sequence_of_hex_number_in_memory[i])
+            return false;
+    }
+
+    return true;
 }
 
 void swap_LE_BE_by_pointer(uint32_t& value) {
@@ -17,7 +21,7 @@ void swap_LE_BE_by_pointer(uint32_t& value) {
     for (int i = 0, j = sizeof(uint32_t) - 1; i < 2; i++, j--) {
         uint8_t temp = ptr[i];
         ptr[i] = ptr[j];
-        ptr[j] = temp; // or there can i use std::swap(ptr[i], ptr[j])
+        ptr[j] = temp; // or there i can use std::swap(ptr[i], ptr[j])
     }
 }
 
