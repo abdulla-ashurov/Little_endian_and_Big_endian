fb52a83e7bc07f0d37c39ef8225d8bc7c90472d2
abdulla.ashurov <abdulla.ashurov@dsr-corporation.com>
2022-08-25 20:10:50 +0500

    Refactor code

--- main.cpp
+++ main.cpp
@@ -23,7 +23,7 @@ void swap_LE_BE_by_pointer(uint32_t &value) {
 }
 
 void swap_LE_BE_by_bitwise(uint32_t &value) {
-    value = (value << 24 | value >> 24) | ((value << 16 >> 24 << 16) | (value >> 16 << 24 >> 16));
+    value = (value << 24 | value >> 24) | (((value & 0x00FF0000) >> 8) | ((value & 0x0000FF00) << 8));
 }
 
 int main()
